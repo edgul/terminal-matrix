1cdf155451ad492d1058acf0f43b64066e6d7b22
edgul <ed.guloien@gmail.com>
2024-10-04 17:51:48 -0400

    primitive multiple columns with ascii only

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,71 @@
 use std::{thread, time};
-
-//use rand::prelude::*;
-
 use rand;
 use rand::Rng;
+
+static BCHAR : char = ' ';
+static COLUMNS: usize = 90; 
 
 fn clear_term() {
     print!("\x1Bc");
 }
 
-fn main() {
-    println!("Hello, world!");
-    clear_term();
+fn random_number(n : u32) -> u32 {
+    rand::thread_rng().gen_range(0..n)
+}
 
-    let frame_period = time::Duration::from_millis(100);
+fn random_ascii() -> u8 {
+    // todo: support unicode
+    rand::thread_rng().gen_range(33..126)
+}
+
+fn col_height(m: &Vec<Vec<char>>, c: usize) -> usize {
+    let mut count = 0;
+    for row in m.iter() {
+        if row[c] == BCHAR {
+            return count;
+        }
+        count += 1;
+    }
+    return count
+}
+
+fn main() {
+    println!("wake up, neo");
+
+    let quit_timer = false;
+
+    let mut matrix : Vec<Vec<char>> = Vec::new();
+    matrix.push(vec![BCHAR; COLUMNS]);
+
+    let frame_period = time::Duration::from_millis(15);
     let animation_length = time::Duration::from_millis(5000); // 5 sec
     let start = time::Instant::now();
-    loop { // animation loop
-        
-        // generate random unicode value
-        let num = rand::thread_rng().gen_range(0..800); // todo: add exclusion list
-        println!("{}", std::char::from_u32(num).unwrap_or('x'));
-       
-        // frame length control and exit
-        thread::sleep(frame_period);
-        if start.elapsed() >= animation_length {
-            break;
+
+    // paint loop
+    loop {
+        clear_term();
+
+        // add new char to the matrix
+        let new_char = random_ascii() as char; 
+        let col = random_number(COLUMNS as u32) as usize;
+        let row = col_height(&matrix, col);
+        if row == matrix.len() {
+            matrix.push(vec![BCHAR; COLUMNS]);
+        }
+        matrix[row][col] = new_char;
+
+        // paint it
+        for row in matrix.iter() {
+            let s : String = row.into_iter().collect();
+            println!("{}", s);
+        }  
+
+        thread::sleep(frame_period); // animation speed
+
+        if quit_timer {
+            if start.elapsed() >= animation_length {
+                break;
+            }
         }
     }
 }
